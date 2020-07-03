9b7b31455b13dccf9272ad9db828c31d053dc75c
maath <matheuskousen@gmail.com>
2020-07-03 18:38:46 -0300

    add commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./sass/index.scss";
-
+// add  slide show
 import SlideShow from "./sass/Layout/Header/slide";
 import "./sass/Layout/Navbar/OpenMenu";
 window.addEventListener("load", SlideShow);
