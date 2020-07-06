9ca2cd20b255044efba88a8a5f13779d0dd61c26
Henrique <henrique-lima-neves@hotmail.com>
2020-07-06 17:49:14 -0300

    kkK

--- src/sass/Layout/main/cursos/data.js
+++ src/sass/Layout/main/cursos/data.js
@@ -1,23 +1,18 @@
-import one from "../../../../../public/assets/img/cursos/excel.png";
-import two from "../../../../../public/assets/img/cursos/word.png";
-import three from "../../../../../public/assets/img/cursos/power.png";
-import four from "../../../../../public/assets/img/cursos/redesocail.jpeg";
-import five from "../../../../../public/assets/img/cursos/english.gif";
-import six from "../../../../../public/assets/img/utils/006.jpeg";
-import seven from "../../../../../public/assets/img/utils/007.jpeg";
-import eight from "../../../../../public/assets/img/utils/008.jpeg";
-import nine from "../../../../../public/assets/img/utils/009.jpeg";
-import ten from "../../../../../public/assets/img/utils/010.jpeg";
-import eleven from "../../../../../public/assets/img/utils/011.jpeg";
-import twelve from "../../../../../public/assets/img/utils/012.jpeg";
-import therteen from "../../../../../public/assets/img/utils/013.jpeg";
-import fourteem from "../../../../../public/assets/img/utils/014.jpeg";
-import fifteen from "../../../../../public/assets/img/utils/015.jpeg";
-import sixteen from "../../../../../public/assets/img/utils/016.jpeg";
-import seventeen from "../../../../../public/assets/img/utils/016.jpeg";
-import eighteen from "../../../../../public/assets/img/utils/018.jpeg";
-import nineteen from "../../../../../public/assets/img/utils/019.jpeg";
-import twenty from "../../../../../public/assets/img/utils/020.jpeg";
+import one from "../../../../../public/assets/img/utils/inicio.jpeg";
+import two from "../../../../../public/assets/img/utils/second.jpeg";
+import three from "../../../../../public/assets/img/utils/terceiro.jpeg";
+import four from "../../../../../public/assets/img/utils/quarto.jpeg";
+import five from "../../../../../public/assets/img/utils/quinto.jpeg";
+import six from "../../../../../public/assets/img/utils/sexto.jpeg";
+import seven from "../../../../../public/assets/img/utils/setimo.jpeg";
+import eight from "../../../../../public/assets/img/utils/oitavo.jpeg";
+import nine from "../../../../../public/assets/img/utils/nono.jpeg";
+import ten from "../../../../../public/assets/img/utils/Inglês1.jpeg";
+import eleven from "../../../../../public/assets/img/utils/Ingles2.jpeg";
+import twelve from "../../../../../public/assets/img/utils/Ingles3.jpeg";
+import therteen from "../../../../../public/assets/img/utils/ingles4.jpeg";
+import fourteem from "../../../../../public/assets/img/utils/ingles5.jpeg";
+import fifteen from "../../../../../public/assets/img/utils/ingles6.jpeg";
 
 const Images = [
   one,
@@ -35,11 +30,6 @@ const Images = [
   therteen,
   fourteem,
   fifteen,
-  sixteen,
-  seventeen,
-  eighteen,
-  nineteen,
-  twenty,
 ];
 
 export default Images;
