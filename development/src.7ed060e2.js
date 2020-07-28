13062535f3457ef88dede8580a9dd5298b989192
matheus kousen <matheuskousen@gmail.com>
2020-07-28 20:24:42 -0300

    new up

--- development/src.7ed060e2.js
+++ development/src.7ed060e2.js
@@ -17356,7 +17356,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54486" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52910" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
